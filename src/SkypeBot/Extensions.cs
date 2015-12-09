97f370e0f7d2775560f83bd0f5f8da1d6e82b9d7
Matthew King <mking@mking.io>
2015-12-09 11:07:24 +0800

    Add an extension method for use within handlers.

---
+++ src/SkypeBot/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SkypeBot
+{
+    public static class Extensions
+    {
+        public static bool ContainsIgnoreCase(this string target, string value)
+        {
+            return target.IndexOf(value, StringComparison.OrdinalIgnoreCase) != -1;
+        }
+    }
+}
